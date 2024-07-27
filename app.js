8953d87206e784d33de80ef69992400d95764c38
Sergey86fr <sergsfree@gmail.com>
2024-07-27 16:57:50 +0500

    edit_timer

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
-let second = 0;
-let timerId;
+let [hours, minutes, seconds, milliseconds] = [0, 0, 0, 0];
+let timerId = null;
 
-function updateTimerDisplay () {
-    document.getElementById("timer").innerText = `${second} секунд`;
-}
+// function updateTimerDisplay() {
+//     [hours, minutes, seconds, milliseconds] = [0, 0, 0, 0]
+//     // document.getElementById("timer").innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+// }
 
-function startTimer () {
-    if(!timerId) {
-        timerId = setInterval(() => {
-            second++
-            updateTimerDisplay()
-        },1000)
+function startTimer() {
+    if (timerId !== null) {
+        clearInterval(timerId);
     }
-}
-function stopTimer () {
+        timerId = setInterval(() => {
+
+            displayTimer();
+            // updateTimerDisplay();
+        }, 10)
+    }
+function stopTimer() {
     clearInterval(timerId);
-    timerId = null;
+   
 }
-function resetTimer () {
+function resetTimer() {
     stopTimer();
-    second = 0;
-    updateTimerDisplay();
+    [hours, minutes, seconds, milliseconds] = [0, 0, 0, 0];
+    document.getElementById("timer").innerHTML = "00 : 00 : 00 : 000";
 }
+
+function displayTimer() {
+    milliseconds += 10;
+    if (milliseconds == 1000) {
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60) {
+            seconds = 0;
+            minutes ++;
+            if(minutes == 60) {
+                minutes = 0;
+                hours ++;
+            }
+        } 
+    }
+    
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
+
+    document.getElementById("timer").innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+}
+
 
 document.getElementById("startButton").addEventListener("click", startTimer);
 document.getElementById("stopButton").addEventListener("click", stopTimer);
